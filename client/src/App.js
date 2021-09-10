ae39ec4b8d61df90cddd889c09ea1336ebc8d620
IslombekAdulakhatov <36397876+IslombekAdulakhatov@users.noreply.github.com>
2021-09-09 22:57:51 -0700

    About me page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,43 @@
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Home from './components/Home';
+import AboutIslom from './components/Islom/AboutIslom';
 
-function App() {
+export default function App() {
   return (
     <Router>
-      <nav>
-        <Link to='/'>Home</Link>
-      </nav>
-      <Switch>
-        <Route path='/'>
-          <Home />
-        </Route>
-      </Switch>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/islom">Islombek Abdulakhatov - SCRUM Master</Link>
+            </li>
+            
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/islom">
+            <AboutIslom />
+          </Route>
+          
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
 
-export default App;
+
