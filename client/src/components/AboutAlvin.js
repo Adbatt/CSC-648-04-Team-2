7327b2c0c2e0769892be2c91023793bf437bd737
alvin to <ato5@mail.sfsu.edu>
2021-09-12 00:06:09 -0700

    test

--- client/src/components/AboutAlvin.js
+++ client/src/components/AboutAlvin.js
@@ -13,18 +13,5 @@ function AboutAlvin() {
         </div>
     )
 }
-// class Table extends Component{
-//     render(){
-//         return (
-//             <div>
-//                   <h1>Islombek Abdulakhatov cacasc</h1>
-//                 <Avatar
-//                 className="table_avatar"
-//                 src="../components/Islom/Islom.jpg"
-//                 />
-//             </div>
-//         )
-//     }
-// }
 
 export default AboutAlvin;
